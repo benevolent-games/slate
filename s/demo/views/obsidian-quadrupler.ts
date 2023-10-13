4f5287301c010560b0198b0a47713af7514731d6
Chase Moskal <chasemoskal@gmail.com>
2023-10-13 14:03:13 -0700

    tweak: obsidian demo syntax

--- s/demo/views/obsidian-quadrupler.ts
+++ s/demo/views/obsidian-quadrupler.ts
@@ -3,17 +3,18 @@ import {css} from "lit"
 import {obsidian} from "../frontend.js"
 import {html} from "../../shiny/quartz.js"
 
-const settings = {
-	name: "quadrupler",
-	styles: css`span {color: yellow}`,
-}
+const name = "quadrupler"
+const styles = css`span { color: yellow }`
 
-export const ObsidianQuadrupler = obsidian(settings, use => (start: number) => {
+export const ObsidianQuadrupler = obsidian({name, styles}, use =>
+		(start: number) => {
+
 	const count = use.signal(start)
+	const increase = () => count.value *= 4
 
 	return html`
 		<span>${count}</span>
-		<button @click=${() => count.value *= 4}>quadrupler</button>
+		<button @click=${increase}>quadrupler</button>
 	`
 })
 
