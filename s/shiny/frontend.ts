aecdd621622f164a9d36ae6e214ad186468ebf6e
Chase Moskal <chasemoskal@gmail.com>
2023-10-14 01:14:16 -0700

    sketch: initial part of portable_frontend

--- s/shiny/frontend.ts
+++ s/shiny/frontend.ts
@@ -5,8 +5,10 @@ import {prepare_carbon} from "./carbon.js"
 import {prepare_oxygen} from "./oxygen.js"
 import {prepare_quartz} from "./quartz.js"
 import {mixin} from "../base/helpers/mixin.js"
+import {LightRenderer} from "./parts/types.js"
 import {prepare_obsidian} from "./obsidian.js"
 import {BaseElementClass} from "../base/element.js"
+import { RequirementGroup, requirement } from "../tools/requirement.js"
 
 export const prepare_frontend = <C extends Context>(context: C) => ({
 	oxygen: prepare_oxygen(context),
@@ -20,5 +22,19 @@ export const prepare_frontend = <C extends Context>(context: C) => ({
 			.to(mixin.signals(context.signals))
 			.done()
 	),
+})
+
+export const portable_frontend = <C extends Context>() => ({
+	provide: <G extends RequirementGroup<C, any>>(context: C, group: G) => (
+		requirement.provide(context)(group)
+	),
+
+	oxygen: (renderer: LightRenderer<C>) => (
+		(context: C) => prepare_oxygen(context)(renderer)
+	),
+
+	// // TODO
+	// finish carbon, quartz, obsidian, and component,
+	// and implement use.prepare() so we can use provide on views
 })
 
