40f401abe126e7d553edf267a31d6e368ad8f95a
Chase Moskal <chasemoskal@gmail.com>
2023-10-14 22:16:29 -0700

    add: apply func for signals

--- s/base/helpers/apply.ts
+++ s/base/helpers/apply.ts
@@ -5,10 +5,11 @@ import {mixin} from "./mixin.js"
 import {ob} from "../../tools/ob.js"
 import {Flat} from "../../flatstate/flat.js"
 import {BaseElementClasses} from "../element.js"
+import {SignalTower} from "../../signals/tower.js"
 
 export namespace apply {
 
-	export const theme = (
+	export const css = (
 		(theme: CSSResultGroup) => (
 			<E extends BaseElementClasses>(elements: E) => (
 				ob.map(elements, Element => mixin.css(theme)(Element))
@@ -20,6 +21,14 @@ export namespace apply {
 		(flat: Flat) => (
 			<E extends BaseElementClasses>(elements: E) => (
 				ob.map(elements, (Element: any) => mixin.flat(flat)(Element))
+			)
+		)
+	)
+
+	export const signals = (
+		(tower: SignalTower) => (
+			<E extends BaseElementClasses>(elements: E) => (
+				ob.map(elements, (Element: any) => mixin.signals(tower)(Element))
 			)
 		)
 	)
