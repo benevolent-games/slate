e4e574b5bd9b07602516375e9e2af4a83b80407c
Chase Moskal <chasemoskal@gmail.com>
2023-10-20 17:37:14 -0700

    add: deep_equal and deep_freeze

--- s/tests.test.ts
+++ s/tests.test.ts
@@ -4,11 +4,13 @@ import clone from "./tools/clone/clone.test.js"
 import signals from "./signals/signals.test.js"
 import flatstate from "./flatstate/flat.test.js"
 import debounce from "./tools/debounce/debounce.test.js"
+import deepEqual from "./tools/deep_equal/deep_equal.test.js"
 
 export default <Suite>{
 	clone,
 	signals,
 	debounce,
 	flatstate,
+	deepEqual,
 }
 
