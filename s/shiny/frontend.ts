391181ff63fcda55592b8c017a8d2a660c9bd7ef
Chase Moskal <chasemoskal@gmail.com>
2023-10-27 19:03:02 -0700

    refine: prepare_frontend with doc-comments

--- s/shiny/frontend.ts
+++ s/shiny/frontend.ts
@@ -7,7 +7,7 @@ import {prepare_quartz} from "./units/quartz.js"
 import {prepare_obsidian} from "./units/obsidian.js"
 import {register_to_dom} from "../base/helpers/register_to_dom.js"
 
-export function prepare_frontend<C extends Context>(context?: C | undefined) {
+export function prepare_frontend<C extends Context>(context?: C) {
 	const shell = new Shell<C>(context)
 	const set_context = (c: C) => { shell.context = c }
 
@@ -15,9 +15,17 @@ export function prepare_frontend<C extends Context>(context?: C | undefined) {
 		shell,
 		set_context,
 		register_to_dom,
+
+		/** light-dom web component */
 		oxygen: prepare_oxygen(shell),
+
+		/** shadow-dom web component */
 		carbon: prepare_carbon(shell),
+
+		/** light-dom lit view */
 		quartz: prepare_quartz(shell),
+
+		/** shadow-dom lit view */
 		obsidian: prepare_obsidian(shell),
 	}
 }
