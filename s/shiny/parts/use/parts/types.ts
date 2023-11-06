028a746e14bad9457895a62db684161fe745b937
Chase Moskal <chasemoskal@gmail.com>
2023-11-06 05:19:43 -0800

    add: InitReturn helper type

--- s/shiny/parts/use/parts/types.ts
+++ s/shiny/parts/use/parts/types.ts
@@ -5,3 +5,9 @@ export type SetupFn = () => Setdown
 export type InitResult<R> = [R, Setdown]
 export type InitFn<R> = () => InitResult<R>
 
+export type InitReturn<I extends InitFn<any>> = (
+	I extends InitFn<infer R>
+		? R
+		: never
+)
+
