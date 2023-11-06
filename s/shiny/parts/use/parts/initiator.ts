f76edda5582a09d47ca70d664d5c266bc6c86f72
Chase Moskal <chasemoskal@gmail.com>
2023-11-05 21:35:13 -0800

    rename: initiator.dispose to initiator.deinit

--- s/shiny/parts/use/parts/initiator.ts
+++ s/shiny/parts/use/parts/initiator.ts
@@ -4,14 +4,14 @@ import {Constructor} from "../../../../tools/constructor.js"
 
 /** a class intended to be used with use.init via initiate() function */
 export abstract class Initiator {
-	abstract dispose(): void
+	abstract deinit(): void
 }
 
 /** initiate an instance of an initiator class (helpful for use.init) */
 export function initiate<C extends Constructor<Initiator>>(C: C) {
 	return (...params: ConstructorParameters<C>): InitFunction<InstanceType<C>> => () => {
 		const instance = new C(...params) as InstanceType<C>
-		return [instance, () => instance.dispose()]
+		return [instance, () => instance.deinit()]
 	}
 }
 
