2175eed9043687b3cd67cb536ec9fdc2d9c381b4
Chase Moskal <chasemoskal@gmail.com>
2023-11-11 01:44:38 -0800

    export: 'flatstate' and 'signal'

--- s/shiny/state.ts
+++ s/shiny/state.ts
@@ -7,3 +7,6 @@ export const flat = new Flat()
 export const signals = new SignalTower()
 export const watch = new WatchTower()
 
+export const flatstate = flat.state.bind(flat)
+export const signal = signals.signal.bind(signals)
+
