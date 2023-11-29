4172615ee43e7ce9ca349f0e13a90a273f717f67
Chase Moskal <chasemoskal@gmail.com>
2023-11-29 00:43:11 -0800

    op: allow DirectiveResult in render_op

--- s/op/prep_render_op.ts
+++ s/op/prep_render_op.ts
@@ -1,13 +1,17 @@
 
 import {Op} from "./op.js"
 import type {TemplateResult} from "lit"
+import type {DirectiveResult} from "lit/async-directive.js"
 
 export function prep_render_op({loading, error}: {
 		loading: () => TemplateResult
 		error: (reason: string) => TemplateResult
 	}) {
 
-	return function render_op<X>(op: Op.For<X>, on_ready: (value: X) => TemplateResult | void) {
+	return function render_op<X>(
+			op: Op.For<X>,
+			on_ready: (value: X) => TemplateResult | DirectiveResult | void,
+		) {
 		return Op.select(op, {
 			loading,
 			error,
