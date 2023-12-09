1ce4b9aa2a043476ce9fe624d16efc4a0c15db5e
Chase Moskal <chasemoskal@gmail.com>
2023-12-08 17:42:42 -0800

    tools: add more 'is' guards

--- s/tools/is.ts
+++ s/tools/is.ts
@@ -1,13 +1,27 @@
 
 export const is = {
+	void: (x: any): x is (undefined | null) =>
+		x === undefined || x === null,
 
-	object: <X>(x: X): x is NonNullable<X> & object =>
+	defined: <X>(x: X): x is NonNullable<X> =>
+		x !== undefined && x !== null,
+
+	boolean: (x: any): x is boolean =>
+		typeof x === "boolean",
+
+	number: (x: any): x is number =>
+		typeof x === "number",
+
+	string: (x: any): x is string =>
+		typeof x === "string",
+
+	bigint: (x: any): x is bigint =>
+		typeof x === "bigint",
+
+	object: <X>(x: X): x is object & NonNullable<X> =>
 		typeof x === "object" && x !== null,
 
 	array: (x: any | any[]): x is any[] =>
 		Array.isArray(x),
-
-	defined: <X>(x: X): x is NonNullable<X> =>
-		x !== undefined && x !== null,
 }
 
