8f74ac105dc967f2f720abcb8b7b9676b545afde
Chase Moskal <chasemoskal@gmail.com>
2024-06-25 00:12:42 -0700

    add: defaultTheme

--- s/nexus/context.ts
+++ s/nexus/context.ts
@@ -1,21 +1,23 @@
 
 import {css, CSSResultGroup} from "lit"
 
+export const defaultTheme: CSSResultGroup = css`
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+
+		scrollbar-width: thin;
+		scrollbar-color: #888 transparent;
+	}
+
+	::-webkit-scrollbar { width: 8px; }
+	::-webkit-scrollbar-track { background: transparent; }
+	::-webkit-scrollbar-thumb { background: #333; border-radius: 1em; }
+	::-webkit-scrollbar-thumb:hover { background: #444; }
+`
+
 export class Context {
-	theme: CSSResultGroup = css`
-		* {
-			margin: 0;
-			padding: 0;
-			box-sizing: border-box;
-
-			scrollbar-width: thin;
-			scrollbar-color: #888 transparent;
-		}
-
-		::-webkit-scrollbar { width: 8px; }
-		::-webkit-scrollbar-track { background: transparent; }
-		::-webkit-scrollbar-thumb { background: #333; border-radius: 1em; }
-		::-webkit-scrollbar-thumb:hover { background: #444; }
-	`
+	theme = defaultTheme
 }
 
