3e73e6d0c4c545d1ec3f73a5a47da62384055eae
Chase Moskal <chasemoskal@gmail.com>
2024-07-13 16:34:02 -0700

    add: deep.clone reference

--- s/tools/deep/deep.ts
+++ s/tools/deep/deep.ts
@@ -1,8 +1,10 @@
 
+import {clone} from "../clone/clone.js"
 import {deepEqual} from "./parts/equal.js"
 import {deepFreeze} from "./parts/freeze.js"
 
 export const deep = {
+	clone,
 	equal: deepEqual,
 	freeze: deepFreeze,
 }
