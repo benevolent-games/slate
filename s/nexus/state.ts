b7a9899cacb06948555ed12c6a252b0bf7825f90
Chase Moskal <chasemoskal@gmail.com>
2024-08-20 12:22:36 -0700

    export: opSignal helper

--- s/nexus/state.ts
+++ s/nexus/state.ts
@@ -10,5 +10,6 @@ export const watch = new WatchTower(signals)
 
 export const flatstate = flat.state.bind(flat)
 export const signal = signals.signal.bind(signals)
+export const opSignal = signals.op.bind(signals)
 export const reactor = new Reactor(flat, signals)
 
