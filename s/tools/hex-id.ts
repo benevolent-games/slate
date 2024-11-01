a671237e243ac75968b76b2ceb7e50de6de0323b
Chase Moskal <chasemoskal@gmail.com>
2024-11-01 11:22:31 -0700

    rename: 'generate_id' to 'hexId'

--- s/tools/hex-id.ts
+++ s/tools/hex-id.ts
@@ -1,9 +1,12 @@
 
-export function generate_id(bytes = 32) {
+export function hexId(bytes = 32) {
 	const array = new Uint8Array(bytes)
 	const random = crypto.getRandomValues(array)
 	return [...random]
 		.map(b => b.toString(16).padStart(2, "0"))
 		.join("")
 }
+
+/** @deprecated renamed to `hexId` */
+export const generate_id = hexId
 
