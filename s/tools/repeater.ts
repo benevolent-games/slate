2122d65aaaf1022acaf9157fd945f8c8efffd7ca
Chase Moskal <chasemoskal@gmail.com>
2024-11-03 11:33:11 -0800

    add: tool 'repeater'

---
+++ s/tools/repeater.ts
@@ -0,0 +1,24 @@
+
+export class Repeater {
+	active = true
+
+	constructor(public milliseconds: number, public fn: () => Promise<void>) {}
+
+	async execute() {
+		if (this.active === true) {
+			await this.fn()
+			setTimeout(() => this.execute(), this.milliseconds)
+		}
+	}
+
+	stop() {
+		this.active = false
+	}
+}
+
+export function repeater(milliseconds: number, fn: () => Promise<void>) {
+	return new Repeater(milliseconds, fn)
+}
+
+repeater.hz = (hertz: number, fn: () => Promise<void>) => repeater(1000 / hertz, fn)
+
