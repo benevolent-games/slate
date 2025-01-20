f5a60b40fcbaebe8acbe32daccfe929dca97726c
Chase Moskal <chasemoskal@gmail.com>
2025-01-19 19:19:19 -0800

    add: ev listener options

--- s/tools/ev.ts
+++ s/tools/ev.ts
@@ -6,11 +6,11 @@ export type EvListener = LitListener | ((...args: any[]) => void)
 export type Ev = Pojo<EvListener>
 
 /** attach event listeners to dom elements */
-export function ev<E extends Ev>(target: EventTarget, events: E) {
+export function ev<E extends Ev>(target: EventTarget, events: E, options?: AddEventListenerOptions) {
 	const entries = Object.entries(events)
 
 	for (const [event, listener] of entries)
-		target.addEventListener(event, listener)
+		target.addEventListener(event, listener, options)
 
 	return function dispose() {
 		for (const [event, listener] of entries)
