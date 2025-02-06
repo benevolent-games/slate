7e7797c7c5850f3d755a91a140a9885faffda0e9
Chase Moskal <chasemoskal@gmail.com>
2025-02-05 19:11:12 -0800

    improve: barname demos

--- s/tools/data/barname/demo.ts
+++ s/tools/data/barname/demo.ts
@@ -1,12 +1,24 @@
 
+import {Hex} from "../hex.js"
+import {Badge} from "./badge.js"
+import {Bytes} from "../bytes.js"
 import {Barname} from "./barname.js"
 
-for (const _ of Array(20)) {
+for (const _ of Array(10)) {
 	const string = Barname.random(4)
 	const bytes = Barname.bytes(string)
 	const string2 = Barname.string(bytes)
-	console.log("_" + string)
+	console.log(string)
 	if (string !== string2)
-		throw new Error("barname fail")
+		throw new Error("barname fails equality check")
+}
+
+for (const _ of Array(10)) {
+	const bytes = Bytes.random(32)
+	const badge = new Badge(bytes)
+	const bytes2 = badge.bytes
+	console.log(badge.string)
+	if (Hex.string(bytes) !== Hex.string(bytes2))
+		throw new Error("badge fails equality check")
 }
 
