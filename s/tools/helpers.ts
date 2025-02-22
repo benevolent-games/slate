fc027fef2ea10e591dd789be0e6fb71c68ea2e2c
Chase Moskal <chasemoskal@gmail.com>
2025-02-22 00:10:06 -0800

    add: 'maybe' helper

--- s/tools/helpers.ts
+++ s/tools/helpers.ts
@@ -1,6 +1,15 @@
 
 import {Signal} from "../signals/signal.js"
 
+export function maybe<X, R>(x: X, fn: (x: X) => R) {
+	const y = x instanceof Signal
+		? x.value
+		: x
+	return y
+		? fn(y)
+		: undefined
+}
+
 export function whereby<X, R>(
 		x: X | undefined | null | Signal<X | undefined | null>,
 		fn: (x: X) => R,
