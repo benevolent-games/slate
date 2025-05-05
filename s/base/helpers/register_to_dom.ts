e0009f39b1f52ddf4eed7fa3d770eb0fef89e65e
Chase Moskal <chasemoskal@gmail.com>
2025-05-04 23:53:41 -0700

    enhance: register_to_dom now auto-upgrades

--- s/base/helpers/register_to_dom.ts
+++ s/base/helpers/register_to_dom.ts
@@ -4,18 +4,31 @@ import {HTMLElementClasses} from "../element.js"
 
 export type RegistrationOptions = {
 	soft: boolean
+	upgrade: boolean
 }
 
 export function register_to_dom<E extends HTMLElementClasses>(
 		elements: E,
-		options: RegistrationOptions = {soft: false},
+		options: Partial<RegistrationOptions> = {},
 	) {
+
+	const soft = options.soft ?? false
+	const upgrade = options.upgrade ?? true
 
 	for (const [name, Element] of Object.entries(elements)) {
 		const tag = dashify(name)
+		const already = customElements.get(tag)
 
-		if (!options.soft || customElements.get(tag) === undefined)
-			customElements.define(tag, Element)
+		if (soft && already)
+			continue
+
+		customElements.define(tag, Element)
+
+		if (upgrade)
+			document.querySelectorAll(tag).forEach(element => {
+				if (element.constructor === HTMLElement)
+					customElements.upgrade(element)
+			})
 	}
 }
 
